99e192c69065272e2d49472867a24f7f888ed6a8
Michael Rose <rosemikey13@aol.com>
2023-05-08 09:53:42 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import logo from "./assets/Logo.png"
+
+function App() {
+  return (
+    <>
+      <div className='mainPage'>
+        <img src={logo} className="logoImg" />
+      </div>
+    </>
+  )
+}
+
+export default App
